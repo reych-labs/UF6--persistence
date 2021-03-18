c5c0a2154e583217f785f06fe5f00c71665610ab
Raquel <ralaman@institutmvm.cat>
2021-03-19 00:49:10 +0100

    Modular Application - Utility methods

--- cat.mvm.modular.contracts/src/cat/mvm/modular/contracts/LoveCalculator.java
+++ cat.mvm.modular.contracts/src/cat/mvm/modular/contracts/LoveCalculator.java
@@ -1,5 +1,8 @@
 package cat.mvm.modular.contracts;
 
+import java.util.Random;
+import java.text.Normalizer;
+
 @FunctionalInterface
 public interface LoveCalculator {
     public static final int MAX_VALUE = 100;
@@ -7,5 +10,28 @@ public interface LoveCalculator {
 
     public abstract int getCompatibilityBetween(String name1, String name2); //interface funcional (SAM - Single Abstract Method)
 
+    /**
+     * els tipus fills no estan obligats a sobreescriure'ls
+     */
+    public default int getIncompatibilityBetween(String name1, String name2) {
+        return nextInt(LoveCalculator.MAX_VALUE);
+    }
 
+    private int nextInt(int bound){
+        var rnd = new Random();
+        return rnd.nextInt(bound + 1);
+    }
+
+    /**
+     * Utility method
+     * @param name
+     * @return String
+     */
+    public static String normalize(String name){
+        return normalizeNFD(name).replaceAll("\\p{Mn}", "");
+    }
+
+    private static String normalizeNFD(String name){
+        return Normalizer.normalize(name, Normalizer.Form.NFD);//NFD: Forma Normal Disyuntiva
+    }
 }
--- cat.mvm.modular.ui/src/cat/mvm/modular/ui/Application.java
+++ cat.mvm.modular.ui/src/cat/mvm/modular/ui/Application.java
@@ -9,6 +9,8 @@ import java.util.Random;
 public class Application {
     public static void main(String[] args){
         var lc1 = new RandomLoveCalculator();
+        System.out.println(LoveCalculator.normalize("Alamán"));
+        System.out.println(LoveCalculator.normalize("García"));
         System.out.println(lc1.getCompatibilityBetween("Josep", "Maria"));
 
         var lc2 = new BasicLoveCalculator();
